15cbf82cc78d5a02257937edec55f09ee05a42ae
Zac <zac.davies@fdmgroup.com>
2020-12-18 19:10:51 +0000

    Added all db functionality in handler, and handler unit tests. Also added mock images

--- src/handlers/imageHandler.js
+++ src/handlers/imageHandler.js
@@ -1,24 +1,152 @@
 const {MongoClient} = require('mongodb');
+const sizeOf = require('image-size');
 
-let dbName = process.env.MONGO_INITDB_DATABASE;
-let dbUser = process.env.MONGO_INITDB_ROOT_USERNAME;
-let dbPassword = process.env.MONGO_INITDB_ROOT_PASSWORD;
+let dbName = process.env.MONGO_INITDB_DATABASE ? process.env.MONGO_INITDB_DATABASE : 'testdb';
+let dbUser = process.env.MONGO_INITDB_ROOT_USERNAME ? process.env.MONGO_INITDB_ROOT_USERNAME : 'testuser';
+let dbPassword = process.env.MONGO_INITDB_ROOT_PASSWORD ? process.env.MONGO_INITDB_ROOT_PASSWORD : 'testpassword';
 
-// functions will be here
+// The following function is primarily used for testing, just so the db tests can run solely based on a fresh database. 
+// As there is only one database being used currently, the original database contents is removed upon starting the tests
+const dropDatabase = async () => {
+    const mongoDbUrl = 'mongodb://' + dbUser + ':' + dbPassword + '@127.0.0.1:27017/';
 
-// should be 6 of them:
+    let connection = await MongoClient.connect(mongoDbUrl, {
+        useNewUrlParser: true
+    });
 
-/* - check db up
-   - get all images
-   - add an image
-   - get one image
-   - delete one image
-   - get and resize an image (DON'T UPDATE)
+    let db = await connection.db(dbName);
 
-*/
+    db.dropDatabase();
 
-// also add drop database here for testing puproses?
+    await connection.close();
+}
+
+const uploadImage = async(name, file) => {
+    const mongoDbUrl = 'mongodb://' + dbUser + ':' + dbPassword + '@127.0.0.1:27017/';
+
+    let connection = await MongoClient.connect(mongoDbUrl, {
+        useNewUrlParser: true
+    });
+
+    let db = await connection.db(dbName);
+
+    const imageFiles = db.collection('imagefiles')
+
+    let dimensions = sizeOf(file);
+
+    const imageUrl = 'http://example.com/' + name + '-' + dimensions.width + 'x' + dimensions.height + '.png';
+
+    const objectToSend = {
+        name: name,
+        width: dimensions.width,
+        height: dimensions.height,
+        url: imageUrl
+    }
+
+    await imageFiles.insertOne(objectToSend);
+
+    await connection.close();
+}
+
+const getOneImage = async(name) => {
+    const mongoDbUrl = 'mongodb://' + dbUser + ':' + dbPassword + '@127.0.0.1:27017/';
+
+    let connection = await MongoClient.connect(mongoDbUrl, {
+        useNewUrlParser: true
+    });
+
+    let db = await connection.db(dbName);
+
+    const imageFiles = db.collection('imagefiles')
+
+    const foundImage = await imageFiles.findOne({
+        name: name
+    });
+
+    await connection.close();
+
+    return foundImage;
+}
+
+const getAllImages = async() => {
+    const mongoDbUrl = 'mongodb://' + dbUser + ':' + dbPassword + '@127.0.0.1:27017/';
+
+    let connection = await MongoClient.connect(mongoDbUrl, {
+        useNewUrlParser: true
+    });
+
+    let db = await connection.db(dbName);
+
+    const imageFiles = db.collection('imagefiles')
+
+    const allImages = await imageFiles.find().toArray();
+
+    await connection.close();
+
+    return allImages;
+}
+
+const resizeImage = async(name, height, width) => {
+    const mongoDbUrl = 'mongodb://' + dbUser + ':' + dbPassword + '@127.0.0.1:27017/';
+
+    let connection = await MongoClient.connect(mongoDbUrl, {
+        useNewUrlParser: true
+    });
+
+    let db = await connection.db(dbName);
+
+    const imageFiles = db.collection('imagefiles')
+
+    const foundImage = await imageFiles.findOne({
+        name: name
+    });
+
+    await connection.close();
+
+    foundImage.width = width;
+    foundImage.height = height;
+
+    return foundImage;
+
+}
+
+const deleteImage = async(name) => {
+    const mongoDbUrl = 'mongodb://' + dbUser + ':' + dbPassword + '@127.0.0.1:27017/';
+
+    let connection = await MongoClient.connect(mongoDbUrl, {
+        useNewUrlParser: true
+    });
+
+    let db = await connection.db(dbName);
+
+    const imageFiles = db.collection('imagefiles');
+
+    await imageFiles.deleteOne({name: name});
+
+    await connection.close();
+}
+
+const healthCheck = async() => {
+    const mongoDbUrl = 'mongodb://' + dbUser + ':' + dbPassword + '@127.0.0.1:27017/';
+
+    let connection = await MongoClient.connect(mongoDbUrl, {
+        useNewUrlParser: true
+    });
+
+    let db = await connection.db(dbName);
+
+    const status = db.slaveOk;
+
+    return status;
+
+}
 
 module.exports = {
-    // functions to export here
+    dropDatabase,
+    uploadImage,
+    getOneImage,
+    getAllImages,
+    resizeImage,
+    deleteImage,
+    healthCheck
 }
--- test/handlers/imageHandler.test.js
+++ test/handlers/imageHandler.test.js
@@ -1,1 +1,69 @@
-// file to test imageHandler. Use Typescript project as basis (mocha not jest)
+const sizeOf = require('image-size');
+const image = './test/resources/imageExample.jpeg';
+const image2 = './test/resources/imageExample2.jpg';
+const {dropDatabase, uploadImage, getOneImage, getAllImages, resizeImage, deleteImage, healthCheck} = require('../../src/handlers/imageHandler');
+
+const expect = require('chai').expect;
+
+describe('Image database calls', () => {
+
+    before(async () => {
+        dropDatabase();
+    })
+
+    after(async () => {
+        dropDatabase();
+    })
+
+    it('should insert a new image', async () => {
+        await uploadImage('testImage', image);
+
+        const dimensions = sizeOf(image);
+
+        const retrievedImage = await getOneImage('testImage');
+
+        expect(retrievedImage.name).to.equal('testImage');
+        expect(dimensions.height).to.equal(retrievedImage.height);
+        expect(dimensions.width).to.equal(retrievedImage.width);
+    })
+
+    it('should get a pre existing image', async () => {
+        const dimensions = sizeOf(image);
+
+        const retrievedImage = await getOneImage('testImage');
+
+        expect(retrievedImage.name).to.equal('testImage');
+        expect(dimensions.height).to.equal(retrievedImage.height);
+        expect(dimensions.width).to.equal(retrievedImage.width);
+    })
+
+    it('should get all images form the database', async () => {
+        await uploadImage('testImage2', image2);
+
+        const retrievedImages = await getAllImages();;
+
+        expect(retrievedImages).to.have.length(2);
+        
+    })
+
+    it('should be able to resize a retrieved image (this does not update the database data)', async() => {
+        const retrievedImage = await resizeImage('testImage', 500, 624);
+
+        expect(retrievedImage.width).to.equal(624);
+        expect(retrievedImage.height).to.equal(500);
+    })
+
+    it("should delete an image and throw 'Image object not found' error when trying to retrieve deleted image", async() => {
+        await deleteImage('testImage2');
+
+        const retrievedImages = await getOneImage('testImage2');
+
+        expect(retrievedImages).to.equal(null);
+    })
+
+    it('should check the db status', async() => {
+        const status = await healthCheck();
+
+        expect(status).to.equal(true);
+    })
+})
